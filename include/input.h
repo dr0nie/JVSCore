87fde5386e1a3281fc9d6723fa88873ae0674206
Marcel <6847936+dr0nie@users.noreply.github.com>
2026-01-05 05:51:51 +0100

    chore: add more key codes to lookup table

--- include/input.h
+++ include/input.h
@@ -13,6 +13,7 @@
 #include <stdlib.h>
 
 #include "jvs.h"
+
 void initInputDefaultConfig(JVSConfig *config);
 #include <linux/input-event-codes.h>
 static StrIntMap keysLookUpMap = {.pairs = {
@@ -25,9 +26,50 @@ static StrIntMap keysLookUpMap = {.pairs = {
                                       {"KEY_ESC", KEY_ESC},
                                       {"KEY_ENTER", KEY_ENTER},
                                       {"KEY_SPACE", KEY_SPACE},
+                                      {"KEY_Q", KEY_Q},
+                                      {"KEY_W", KEY_W},
+                                      {"KEY_E", KEY_E},
+                                      {"KEY_R", KEY_R},
+                                      {"KEY_F", KEY_F},
+                                      {"KEY_Z", KEY_Z},
+                                      {"KEY_Y", KEY_Y},
+                                      {"KEY_T", KEY_T},
+                                      {"KEY_X", KEY_X},
+                                      {"KEY_C", KEY_C},
+                                      {"KEY_V", KEY_V},
+                                      {"KEY_B", KEY_B},
+                                      {"KEY_N", KEY_N},
+                                      {"KEY_M", KEY_M},
+                                      {"KEY_I", KEY_I},
+                                      {"KEY_K", KEY_K},
+                                      {"KEY_J", KEY_J},
+                                      {"KEY_L", KEY_L},
+                                      {"KEY_A", KEY_A},
+                                      {"KEY_S", KEY_S},
+                                      {"KEY_D", KEY_D},
                                       {"KEY_0", KEY_0},
                                       {"KEY_1", KEY_1},
-                                      {"KEY_5", KEY_5}}};
+                                      {"KEY_2", KEY_2},
+                                      {"KEY_3", KEY_3},
+                                      {"KEY_4", KEY_4},
+                                      {"KEY_5", KEY_5},
+                                      {"KEY_6", KEY_6},
+                                      {"KEY_7", KEY_7},
+                                      {"KEY_8", KEY_8},
+                                      {"KEY_8", KEY_8},
+                                      {"KEY_9", KEY_9},
+                                      {"KEY_F1", KEY_F1},
+                                      {"KEY_F2", KEY_F2},
+                                      {"KEY_F3", KEY_F3},
+                                      {"KEY_F4", KEY_F4},
+                                      {"KEY_F5", KEY_F5},
+                                      {"KEY_F6", KEY_F6},
+                                      {"KEY_F7", KEY_F7},
+                                      {"KEY_F8", KEY_F8},
+                                      {"KEY_F9", KEY_F9},
+                                      {"KEY_F10", KEY_F10},
+                                      {"KEY_F11", KEY_F11},
+                                      {"KEY_F12", KEY_F12}}};
 
 int initInput(JVSConfig *config, JVSCapabilities *sentCapabilities, char *name, int analogueFuzz);
 int closeInput();
